ab5828614a3ac7a41d17f9efcb85fd643be543ae
satyam sinha <satyamsinha8158@gmail.com>
2024-06-23 19:49:43 +0530

    updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ gulp.task('sass', function() {
 });
 
 // Minify compiled CSS
-gulp.task('minify-css', ['sass'], function() {
+gulp.task('minify-css', gulp.series('sass'), function() {
   return gulp.src('css/resume.css')
     .pipe(cleanCSS({
       compatibility: 'ie8'
@@ -103,7 +103,7 @@ gulp.task('copy', function() {
 })
 
 // Default task
-gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
+gulp.task('default', gulp.series('sass', 'minify-css', 'minify-js', 'copy'));
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
@@ -115,7 +115,16 @@ gulp.task('browserSync', function() {
 })
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() {
+// gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() {
+//   gulp.watch('scss/*.scss', ['sass']);
+//   gulp.watch('css/*.css', ['minify-css']);
+//   gulp.watch('js/*.js', ['minify-js']);
+//   // Reloads the browser whenever HTML or JS files change
+//   gulp.watch('*.html', browserSync.reload);
+//   gulp.watch('js/**/*.js', browserSync.reload);
+// });
+
+gulp.task('dev', gulp.series('browserSync', 'sass', 'minify-css', 'minify-js'), function() {
   gulp.watch('scss/*.scss', ['sass']);
   gulp.watch('css/*.css', ['minify-css']);
   gulp.watch('js/*.js', ['minify-js']);
